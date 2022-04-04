28498d700db306cc0d554adfa39238ddc81cb636
Альфред Хабибуллин <ARKhabibullin@stud.kpfu.ru>
2022-04-04 12:28:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bubble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива :");
+            int Num = int.Parse(Console.ReadLine());
+            int[] array = new int[Num];
+
+            Console.WriteLine("Введите элементы массива :");
+           
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Work.BubbleSort(array);//сама работа метода
+
+            Console.WriteLine("Отсортированный массив :");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Work.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bubble
+{
+    class Work
+    {
+        public static int[] BubbleSort(int[] array)
+        {
+            int temp;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
